577cfd6cc1f3f74a29e50500e1a2e58a294a52c0
umar5252-g <mromertariq@gmail.com>
2026-03-04 14:42:06 +0500

    create nested objects

--- javascript-basics/script.js
+++ javascript-basics/script.js
@@ -1,24 +1,42 @@
 // console.log("hello world");
 // console.log(5 + 2);
 
-const no1 = 1;
-const no2 = "2";
+// var name = "uamr";
+// var name = "umar khan";
+// console.log(name);
+
+// document.writeln("hello world");
+
+// const no1 = prompt("please enter your age");
+// const no2 = 2;
+// const no3 = 30;
+// const no4 = 4;
+// console.log("your age : ", no1);
+
+// console.log("no1 + no2:", no1 + no2);
+// console.log("no1 + no2: " + no1 + no2);
 // console.log(no1 + no2);
 
-const no3 = 30;
-const no4 = 4;
 // console.log(no3 + no4);
 
-console.log(no1 < no2 && no3 > no4); // true
-console.log(no1 < no2 || no3 > no4); // true
+// console.log(no1 <= no2 && no3 >= no4); // true
+// console.log(no1 <= no2 || no3 >= no4); // true
 
 const user = {
   name: "umar",
   age: "20",
   gender: "male",
   isStudent: true,
+  hobies: ["coding", "gaming", "reading"],
+  education: {
+    degree: "bachelor",
+    field: "computer science",
+    university: "Islamia collage",
+  },
 };
 
-(console.log(user), console.log(user.name));
-console.log(user.age);
-console.log(user.isStudent);
+// (console.log(user), console.log(user.name));
+// console.log(user.age);
+// console.log(user.isStudent);
+// console.log(user.hobies[1]);
+console.log(user.education.university);
