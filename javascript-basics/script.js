0e99a74f89121708e9a2447dd6f30a716ba92c37
umar5252-g <mromertariq@gmail.com>
2026-03-04 14:31:04 +0500

    basic js class 9

--- javascript-basics/script.js
+++ javascript-basics/script.js
@@ -1,2 +1,24 @@
-console.log("hello world");
-console.log(5 + 2);
+// console.log("hello world");
+// console.log(5 + 2);
+
+const no1 = 1;
+const no2 = "2";
+// console.log(no1 + no2);
+
+const no3 = 30;
+const no4 = 4;
+// console.log(no3 + no4);
+
+console.log(no1 < no2 && no3 > no4); // true
+console.log(no1 < no2 || no3 > no4); // true
+
+const user = {
+  name: "umar",
+  age: "20",
+  gender: "male",
+  isStudent: true,
+};
+
+(console.log(user), console.log(user.name));
+console.log(user.age);
+console.log(user.isStudent);
