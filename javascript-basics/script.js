32f661599944a1ff03977d371623937c2cae5e36
umar5252-g <mromertariq@gmail.com>
2026-03-04 14:56:52 +0500

    create an object and display data on the page

--- javascript-basics/script.js
+++ javascript-basics/script.js
@@ -22,21 +22,37 @@
 // console.log(no1 <= no2 && no3 >= no4); // true
 // console.log(no1 <= no2 || no3 >= no4); // true
 
-// const user = {
-//   name: "umar",
-//   age: "20",
-//   gender: "male",
-//   year: "3rd year",
-//   country: "Pakistan",
-//   isStudent: true,
-//   city: "Peshawar",
-//   hobies: ["coding", "gaming", "reading"],
-//   education: {
-//     degree: "bachelor",
-//     field: "computer science",
-//     university: "Islamia collage",
-//   },
-// };
+const user = {
+  name: "umar",
+  age: "20",
+  gender: "male",
+  year: "3rd year",
+  country: "Pakistan",
+  isStudent: true,
+  city: "Peshawar",
+  hobies: ["coding", "gaming", "reading"],
+  education: {
+    degree: "bachelor",
+    field: "computer science",
+    university: "Islamia collage",
+  },
+};
+
+document.writeln(
+  "User Data -->",
+  "\n name: ",
+  user.name,
+  "\n age: ",
+  user.age,
+  "\ngender: ",
+  user.gender,
+  "\n year: ",
+  user.year,
+  "\n country: ",
+  user.country,
+  "\n isStudent: ",
+  user.isStudent,
+);
 
 // (console.log(user), console.log(user.name));
 // console.log(user.age);
