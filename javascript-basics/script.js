b39ae7298ea79ae2d8a2006c47073f5938e6ecbb
umar5252-g <mromertariq@gmail.com>
2026-03-07 12:44:43 +0500

    complete home task

--- javascript-basics/script.js
+++ javascript-basics/script.js
@@ -22,48 +22,80 @@
 // console.log(no1 <= no2 && no3 >= no4); // true
 // console.log(no1 <= no2 || no3 >= no4); // true
 
-const user = {
-  name: "umar",
-  age: "20",
-  gender: "male",
-  year: "3rd year",
-  country: "Pakistan",
-  isStudent: true,
-  city: "Peshawar",
-  hobies: ["coding", "gaming", "reading"],
-  education: {
-    degree: "bachelor",
-    field: "computer science",
-    university: "Islamia collage",
-  },
-};
+// const user = {
+//   name: "umar",
+//   age: "20",
+//   gender: "male",
+//   year: "3rd year",
+//   country: "Pakistan",
+//   isStudent: true,
+//   city: "Peshawar",
+//   hobies: ["coding", "gaming", "reading"],
+//   education: {
+//     degree: "bachelor",
+//     field: "computer science",
+//     university: "Islamia collage",
+//   },
+// };
 
-document.writeln(
-  "User Data -->",
-  "<br> name: ",
-  user.name,
-  "<br> age: ",
-  user.age,
-  "<br> gender: ",
-  user.gender,
-  "<br> year: ",
-  user.year,
-  "<br> country: ",
-  user.country,
-  "<br> isStudent: ",
-  user.isStudent,
-);
+// document.writeln(
+//   "User Data -->",
+//   "<br> name: ",
+//   user.name,
+//   "<br> age: ",
+//   user.age,
+//   "<br> gender: ",
+//   user.gender,
+//   "<br> year: ",
+//   user.year,
+//   "<br> country: ",
+//   user.country,
+//   "<br> isStudent: ",
+//   user.isStudent,
+// );
 
-console.log(
-  "user data -->",
+// console.log(
+//   "user data -->",
 
-  "\n name: ",
-  user.name,
-  "\n age: ",
-  user.age,
-);
+//   "\n name: ",
+//   user.name,
+//   "\n age: ",
+//   user.age,
+// );
 // (console.log(user), console.log(user.name));
 // console.log(user.age);
 // console.log(user.isStudent);
 // console.log(user.hobies[1]);
 // console.log(user.education.university);
+
+// while (num <= 50) {
+//   if (num % 5 == 0) {
+//     console.log(num, " is divisable by 5");
+//     num++;
+//   } else {
+//     num++;
+//   }
+// }
+// do {
+//   console.log("hi my name is umar");
+// } while (num < 1);
+
+// let count = 0;
+// while (num > 0) {
+//   num = Math.floor(num / 10);
+//   count++;
+// }
+// console.log(count);
+
+// for (let i = 10; i > 0; i--) {
+//   console.log(i);
+// }
+
+// let res = 0;
+
+// while (res <= 100) {
+//   let num = Number(prompt("enter a number"));
+//   res += num;
+// }
+
+// console.log("sum exeeded from 100");
