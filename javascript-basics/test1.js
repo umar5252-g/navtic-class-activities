94f303b51023862b61bbdd188949a80a48db54b2
umar5252-g <mromertariq@gmail.com>
2026-03-10 14:21:32 +0500

    test1

---
+++ javascript-basics/test1.js
@@ -0,0 +1,34 @@
+//Q2
+// let a = Number(prompt("enter a number"));
+// let b = Number(prompt("enter another number"));
+// console.log(a + b);
+// console.log(a - b);
+// console.log(a * b);
+// console.log(a / b);
+// console.log(a % b);
+
+//Q3
+// let guessNum = 5;
+// while (true) {
+//   let InputNum = Number(prompt("gues the number"));
+//   if (guessNum !== InputNum) {
+//     console.log("Your guess is wrong");
+//   } else {
+//     console.log("you guess is right");
+//     break;
+//   }
+// }
+
+//Q4
+// let res = 0;
+// while (res <= 100) {
+//   let num = Number(prompt("enter a number"));
+//   res += num;
+// }
+// console.log("sum exeeded from 100");
+
+// let firstname = prompt("enter firstname");
+// let lastname = prompt("enter last name");
+// let email = prompt("enter email");
+// let password = prompt("enter username");
+// let gender = prompt("enter your gender");
