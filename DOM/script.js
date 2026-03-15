ac20a19edce10849b86fa9e11d1cf93cf0ea3d39
umar5252-g <mromertariq@gmail.com>
2026-03-15 21:23:49 +0500

    add remove item and add item features

---
+++ DOM/script.js
@@ -0,0 +1,27 @@
+// let buttonn = document.getElementById("btn");
+// buttonn.addEventListener("click", () => {
+//   alert("button was clicked");
+// });
+
+let list = document.getElementById("list");
+// let newItem = document.createElement("li");
+// newItem.textContent = "New item";
+
+// list.appendChild(newItem);
+
+let item = document.getElementById("item");
+let removeBtn = document.getElementById("remove-btn");
+let replaceBtn = document.getElementById("replace-btn");
+let addBtn = document.getElementById("add-btn");
+
+removeBtn.addEventListener("click", () => {
+  if (item) {
+    list.removeChild(item);
+  }
+});
+
+addBtn.addEventListener("click", () => {
+  let newItem = document.createElement("li");
+  newItem.textContent = "New item";
+  list.appendChild(newItem);
+});
--- sign-up-js/script.js
+++
@@ -1,5 +0,0 @@
-let button = document.getElementById("btn");
-
-button.addEventListener("click", () => {
-  alert("button was clicked");
-});
